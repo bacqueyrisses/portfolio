115b8e4da066ddf13a755f4257c30a4125a343f7
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-06-02 10:20:40 +0200

    test gtag

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS2}`}
+          />  
+          <script
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
