7eff8469bb0af1fe945f847508c855b63ab1daad
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-07-16 16:10:43 +0200

    button update

--- pages/index.js
+++ pages/index.js
@@ -30,17 +30,17 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-            <div className="content" dangerouslySetInnerHTML={{__html: 
+            {/* <div className="content" dangerouslySetInnerHTML={{__html: 
             
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MHP355G');</script>
+            })(window,document,'script','dataLayer','GTM-MHP355G');</script> */}
             
             
   
-            }}></div>
+            {/* }}></div> */}
                 <title>Enzo Bacqueyrisses | Portfolio</title>
                 <meta
                     name="description"
