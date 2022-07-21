29c61da84f0b3c2954e92a5a589e335f8299b0cc
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-07-21 17:12:38 +0200

    hide/show content with selectors

--- src/script.js
+++ src/script.js
@@ -1,27 +1,54 @@
 // Hide classes when hovering buttons
+if (typeof window !== 'undefined') {
+    let activeFilter = null;
+    const filterNotApplicable = (mouseEvent) =>
+        ((window.screen.width > 640 && mouseEvent === 'click') ||
+        (window.screen.width <= 640 &&
+            ['mouseenter', 'mouseleave'].includes(mouseEvent)));
 
-document.querySelectorAll('.selector').forEach((button) => {
-    "mouseenter click".split(" ").forEach(mouseEvent =>
-    button.addEventListener(mouseEvent, (e) => {
-        const filter = e.target.dataset.filter;
-        document.querySelectorAll('.section:not(.' + filter + ')').forEach((contentToHide) => {
-        if ((window.screen.width <= 640) && (mouseEvent === 'click')) {
-            contentToHide.classList.toggle('soft-hide')
-            } else if (window.screen.width > 640) {
-            contentToHide.classList.toggle('soft-hide')
-            } else {
-        return
-    }
+    document.querySelectorAll('.selector').forEach((button) => {
+        'mouseenter click'.split(' ').forEach((mouseEvent) =>
+            button.addEventListener(mouseEvent, (e) => {
+                if (filterNotApplicable(mouseEvent)) {
+                    return;
+                }
 
-})}));
+                const filter = e.target.dataset.filter;
 
-    button.addEventListener('mouseleave', (e) => {
-        const filter = e.target.dataset.filter;
-        if (window.screen.width > 640) {
-        document.querySelectorAll('.section:not(.' + filter + ')').forEach((contentToHide) => {
-            contentToHide.classList.remove('soft-hide');
-            })};
+                if (activeFilter === null) {
+                    activeFilter = applyFilter(filter);
+                    return;
+                }
+
+                if (activeFilter === filter) {
+                    activeFilter = removeFilter();
+                    return;
+                }
+
+                if (activeFilter !== null && activeFilter !== filter) {
+                    removeFilter();
+                    activeFilter = applyFilter(filter);
+                }
+            })
+        );
     });
-});
+}
 
-// Séparer mouseenter sur desktop et click sur desktop et mobile
+const applyFilter = (filter) => {
+    const contentsToHide = document.querySelectorAll(
+        '.section:not(.' + filter + ')'
+    );
+    contentsToHide.forEach((contentToHide) => {
+        contentToHide.classList.add('bye');
+    });
+
+    return filter;
+};
+
+const removeFilter = () => {
+    const contentsToShow = document.querySelectorAll('.bye');
+    contentsToShow.forEach((contentToShow) => {
+        contentToShow.classList.remove('bye');
+    });
+    return null;
+};
