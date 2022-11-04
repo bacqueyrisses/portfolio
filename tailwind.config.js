57934653791c3e01886afa801c8b1a92b457b66a
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-11-04 15:19:36 +0100

    Changed logo size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,6 @@ module.exports = {
       center: true,
     },
     extend: {
-      fontFamily: {
-        'boska': ['Boska', 'serif'],
-        'cabinet': ["'Cabinet Grotesk'", 'sans-serif'],
-        'sentient': ["Sentient", 'serif'],
-      },
-      backgroundImage: {
-        'gradient': "url('/ressources/tw-gradient.png')",
-        'gradientt': "url('/ressources/gradient-two.jpg')",
-      },
       boxShadow: {
         'stripe': '0px 2px 2px 1px rgba(0,0,0,0.2);'
       },
