5e4808140efef70348bca42703fd7fe8e739ec83
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2022-12-13 00:48:28 +0100

    Update dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/script.js',
-],
-  theme: {
-    container: {
-      center: true,
+    content: ["./index.html", "./src/script.js"],
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            boxShadow: {
+                stripe: "0px 2px 2px 1px rgba(0,0,0,0.2);",
+            },
+        },
     },
-    extend: {
-      boxShadow: {
-        'stripe': '0px 2px 2px 1px rgba(0,0,0,0.2);'
-      },
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
