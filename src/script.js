20cecb37f8a6ad40d344cc027804d8d5f8a56892
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2022-12-13 15:26:01 +0100

    Clean JS comments

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// Define them in Local Storage for manual switch and matchMedia for OS preferences
 if (
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
@@ -8,7 +8,7 @@ if (
     document.documentElement.classList.remove("dark");
 }
 
-// Change the icons inside the button based on previous settings
+// Add dark mode behaviour on click
 
 const themeToggleBtn = document.getElementById("theme-toggle");
 
@@ -34,15 +34,8 @@ themeToggleBtn.addEventListener("click", function () {
         }
     }
 });
-// const head = document.getElementsByTagName("head")[0];
-// const meta = document.createElement("meta");
-// meta.name = "theme-color";
 
-// window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
-//     const newColorScheme = event.matches ? (meta.content = "#282c34") : (meta.content = "#fbead1");
-// });
-
-// head.appendChild(meta);
+// Add gesture behaviour for mobile version
 
 let pageWidth = window.innerWidth || document.body.clientWidth;
 let treshold = Math.max(1, Math.floor(0.01 * pageWidth));
@@ -136,186 +129,13 @@ function handleGesture(e) {
     }
 }
 
-// let touchstartX = 0;
-// let touchstartY = 0;
-// let touchendX = 0;
-// let touchendY = 0;
-// let activeFilter = 'mail'
-//
-// const mail = document.getElementById('mail')
-// const mail2 = document.getElementById('mail-2')
-// const mail3 = document.getElementById('mail-3')
-//
-// const gestureZone = document.body;
-//
-// gestureZone.addEventListener('touchstart', function(event) {
-//     touchstartX = event.changedTouches[0].screenX;
-//     touchstartY = event.changedTouches[0].screenY;
-// }, false);
-//
-// gestureZone.addEventListener('touchend', function(event) {
-//     touchendX = event.changedTouches[0].screenX;
-//     touchendY = event.changedTouches[0].screenY;
-//     handleGesture();
-// }, false);
-//
-// function handleGesture() {
-//     if (touchendX < touchstartX) {
-//         if (activeFilter === "mail") {
-//             mail.classList.toggle('b-hidden')
-//             mail2.classList.toggle('b-hidden')
-//             return activeFilter = 'linkedin'
-//         }
-//
-//         if (activeFilter === 'linkedin') {
-//             mail2.classList.toggle('b-hidden')
-//             mail3.classList.toggle('b-hidden')
-//             return activeFilter = 'github'
-//         }
-//
-//         if (activeFilter === 'github') {
-//             mail3.classList.toggle('b-hidden')
-//             mail.classList.toggle('b-hidden')
-//             return activeFilter = 'mail'
-//         }
-//     }
-//
-//     if (touchendX > touchstartX) {
-//         if (activeFilter === "mail") {
-//             mail.classList.toggle('b-hidden')
-//             mail3.classList.toggle('b-hidden')
-//             return activeFilter = 'github'
-//         }
-//
-//         if (activeFilter === 'github') {
-//             mail3.classList.toggle('b-hidden')
-//             mail2.classList.toggle('b-hidden')
-//             return activeFilter = 'linkedin'
-//         }
-//
-//         if (activeFilter === 'linkedin') {
-//             mail2.classList.toggle('b-hidden')
-//             mail.classList.toggle('b-hidden')
-//             return activeFilter = 'mail'
-//         }
-//     }
-//
-// if (touchendY < touchstartY) {
-//     if (activeFilter === "one") {
-//         mail.classList.toggle('b-hidden')
-//         mail2.classList.toggle('b-hidden')
-//         return activeFilter = 'two'
-//     }
-//
-//     if (activeFilter === 'two') {
-//         mail2.classList.toggle('b-hidden')
-//         mail3.classList.toggle('b-hidden')
-//         return activeFilter = 'three'
-//     }
-//
-//     if (activeFilter === 'three') {
-//         mail3.classList.toggle('b-hidden')
-//         mail.classList.toggle('b-hidden')
-//         return activeFilter = 'one'
-//     }
-//
-// }
-//
-// if (touchendY > touchstartY) {
-//     if (activeFilter === "one") {
-//         mail.classList.toggle('b-hidden')
-//         mail2.classList.toggle('b-hidden')
-//         return activeFilter = 'two'
-//     }
-//
-//     if (activeFilter === 'two') {
-//         mail2.classList.toggle('b-hidden')
-//         mail3.classList.toggle('b-hidden')
-//         return activeFilter = 'three'
-//     }
-//
-//     if (activeFilter === 'three') {
-//         mail3.classList.toggle('b-hidden')
-//         mail.classList.toggle('b-hidden')
-//         return activeFilter = 'one'
-//     }
-//
-// }
+// Add custom Safari 15 theme on dark mode change (not necessary with plain bg)
+// const head = document.getElementsByTagName("head")[0];
+// const meta = document.createElement("meta");
+// meta.name = "theme-color";
 
-// if (touchendY === touchstartY) {
-//     console.log('Tap');
-//     alert('tap')
-//
-// }
-// }
+// window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+//     const newColorScheme = event.matches ? (meta.content = "#282c34") : (meta.content = "#fbead1");
+// });
 
-// if (typeof window !== 'undefined') {
-//     // Prerequisites
-//     let activeFilter = null;
-//     const filterNotApplicable = mouseEvent => {
-//         if (
-//             (window.screen.width > 640 && mouseEvent === 'click') ||
-//             (window.screen.width <= 640 && ['mouseenter', 'mouseleave'].includes(mouseEvent))
-//         ) {
-//             return true;
-//         }
-//     };
-//
-//     // Set activeFilter ifs
-//     document.querySelectorAll('.selector').forEach(selector => {
-//         'mouseenter mouseleave click'.split(' ').forEach(mouseEvent => {
-//             if (filterNotApplicable(mouseEvent)) {
-//                 return;
-//             }
-//
-//             selector.addEventListener(mouseEvent, e => {
-//                 const filter = e.target.dataset.filter;
-//
-//                 // activeFilter ifs
-//                 if (activeFilter === null) {
-//                     activeFilter = applyFilter(filter);
-//                     return;
-//                 }
-//
-//                 if (activeFilter === filter) {
-//                     activeFilter = removeFilter(filter);
-//                     return;
-//                 }
-//
-//                 if (activeFilter !== null && activeFilter !== filter) {
-//                     removeFilter(activeFilter);
-//                     activeFilter = applyFilter(filter);
-//                     return;
-//                 }
-//             });
-//         });
-//     });
-//
-//     // Functions
-//     const applyFilter = filter => {
-//         // Hide content
-//         const contentToHide = document.querySelectorAll(`.section:not(.${filter})`);
-//         contentToHide.forEach(content => {
-//             content.classList.add('hide');
-//         });
-//         // Make the selector font-style go italic
-//         document.querySelector(`li[data-filter=${filter}]`).style.fontStyle = 'italic';
-//         // activeFilter = filter
-//         return filter;
-//     };
-//
-//     const removeFilter = filter => {
-//         // Reveal hidden content
-//         const contentToShow = document.querySelectorAll('.hide');
-//         contentToShow.forEach(content => {
-//             content.classList.remove('hide');
-//         });
-//         // Make the selector font-style go normal
-//         document.querySelector(`li[data-filter=${filter}]`).style.fontStyle = 'normal';
-//         // activeFilter = null
-//         return null;
-//     };
-// }
-//
-//
-//
+// head.appendChild(meta);
