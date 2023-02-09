fae7e73f76288c32490ff5ca01edde58d6445667
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-10 00:43:32 +0100

    Add meta description/title, structure _app & _document files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={"h-screen bg-[#e1e6de] px-7 py-3"}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
