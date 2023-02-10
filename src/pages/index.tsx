073bf1a78817f7e4d5fef96d09f68aabc2629768
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-10 13:36:59 +0100

    Change line height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
       <Header />
       <main className={""}>
-        <section className={"mt-16 text-3xl sm:text-4xl"}>
+        <section className={"mt-16 text-[1.65rem] leading-8 sm:text-4xl"}>
           <div className={"font-medium"}>
             Hi, I’m Enzo! <br />A{" "}
             <span className={gooperFont.className}>Web Developer</span>
