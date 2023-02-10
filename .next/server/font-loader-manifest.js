5efad68df224db41674bbdc52707c7decf09a075
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-10 01:07:23 +0100

    Add next swc

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,11 +1,1 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {
-    "/_app": [
-      "static/media/867bce6efedfde96.p.ttf"
-    ],
-    "/": [
-      "static/media/4df0e83118b9a8c9.p.ttf"
-    ]
-  },
-  "app": {}
-}
+self.__FONT_LOADER_MANIFEST={pages:{"/_app":["static/media/867bce6efedfde96.p.ttf"],"/":["static/media/4df0e83118b9a8c9.p.ttf"]},app:{}};
