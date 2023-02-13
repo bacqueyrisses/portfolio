6bf6e6741baa5c82f81c3e00ef2932e5c3df6b9a
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-13 10:58:03 +0100

    Add bg color to html document'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,13 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className={"scroll-smooth"}>
+    <Html
+      lang="en"
+      className={
+        "scroll-smooth bg-secondary text-black dark:bg-[#28282b] dark:text-secondary"
+      }
+    >
       <Head />
-      <body
-        className={
-          "bg-secondary text-black dark:bg-[#28282b] dark:text-secondary"
-        }
-      >
+      <body className={""}>
         <Main />
         <NextScript />
       </body>
