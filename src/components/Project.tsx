fdb8ee7237fc74120e4bdfab9e97bfaa60457c22
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-03-24 16:51:16 +0100

    Handle hovering state on mobile'

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,17 +2,19 @@
 import { Inter } from "@next/font/google";
 import { useEffect, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
+import { isMobile } from "react-device-detect";
 
 export default function Project({ project, index }: any) {
-  const [borderOpacity, setBorderOpacity] = useState(0);
+  const [borderOpacity, setBorderOpacity] = useState(0.05);
+  const [borderHovering, setborderHovering] = useState(false);
   const [scroll, setScroll] = useState<number[]>([]);
 
   useEffect(() => {
-    if (window.screen.width < 900)
-      setScroll([100, 400].map((e) => e * (index + 1)));
-    if (window.screen.width >= 900)
-      setScroll([0, 100].map((e) => e * (index + 1)));
+    isMobile
+      ? setScroll([100, 400].map((e) => e * (index + 1)))
+      : setScroll([0, 600]);
   }, [borderOpacity]);
+
   const handleScroll = () => {
     const position = window.scrollY;
     const positionCorrected = position - scroll[0];
@@ -21,9 +23,9 @@ export default function Project({ project, index }: any) {
     let opacity = positionCorrected / scrollMaxStartingAtZero;
 
     if (opacity >= 1) return setBorderOpacity(1);
-    if (opacity >= 0) return setBorderOpacity(round(opacity, 2));
+    if (opacity > 0) return setBorderOpacity(round(opacity, 2));
 
-    return setBorderOpacity(0);
+    return setBorderOpacity(0.05);
   };
 
   useEffect(() => {
@@ -36,9 +38,27 @@ export default function Project({ project, index }: any) {
 
   return (
     <a
+      onMouseEnter={
+        isMobile
+          ? () => setborderHovering(false)
+          : () => setborderHovering(true)
+      }
+      onMouseLeave={
+        isMobile
+          ? () => setborderHovering(true)
+          : () => setborderHovering(false)
+      }
       href="https://github.com/bacqueyrisses/garaikunde"
-      style={{ border: `2px solid rgba(0, 0, 0, ${borderOpacity})` }}
-      className={`group flex flex-col justify-center rounded-3xl px-4 py-5 transition hover:rounded-3xl dark:border-secondary md:hover:border-2 md:hover:border-opacity-100 md:dark:hover:border-secondary`}
+      style={
+        borderHovering
+          ? {
+              border: `2px solid rgba(0, 0, 0, 1)`,
+            }
+          : {
+              border: `2px solid rgba(0, 0, 0, ${borderOpacity})`,
+            }
+      }
+      className={`borderHover group flex flex-col justify-center rounded-3xl px-4 py-5 transition hover:rounded-3xl hover:border-opacity-100 dark:border-secondary md:hover:border-2 md:dark:hover:border-secondary`}
       target="_blank"
       rel="noopener noreferrer"
     >
