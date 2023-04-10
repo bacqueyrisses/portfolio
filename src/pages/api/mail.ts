e42064343f67b61eeeaf99c0ee99af5dc05f0ac9
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 17:10:12 +0200

    Add mail tracker

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 import fs from "fs";
+import transporter from "./../../services/mailer";
 
 const file = fs.readFileSync(process.cwd() + "/public/images/logo.webp");
 
@@ -8,6 +9,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req);
+  await transporter.sendMail({
+    from: "devzoneapplication@gmail.com",
+    to: "enzo.bacqueyrisses@gmail.com",
+    subject: "Mail lu",
+    html: `<b>${JSON.stringify(req)}</b>`,
+  });
   res.status(200).setHeader("Content-Type", "image/webp").send(file);
 }
