9a4c705db4db3946e934807c1c96f617ab1dc50e
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 16:53:07 +0200

    Change CORS settings

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -2,15 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 import fs from "fs";
 
-// async function cors(req: NextApiRequest, res: NextApiResponse) {
-//   await NextCors(req, res, {
-//     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-//     origin: "*",
-//     optionsSuccessStatus: 200,
-//   });
-// }
-
-// const file = fs.readFileSync(`public/images/logo.webp`);
 const file = fs.readFileSync(process.cwd() + "/public/images/logo.webp");
 
 export default async function handler(
@@ -18,9 +9,4 @@ export default async function handler(
   res: NextApiResponse
 ) {
   res.status(200).setHeader("Content-Type", "image/webp").send(file);
-  // await NextCors(req, res, {
-  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  //   origin: "*",
-  //   optionsSuccessStatus: 200,
-  // });
 }
