5285165f96ebad414444ef3eab234eaf3992f23e
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 16:23:08 +0200

    Change CORS setup

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -10,16 +10,23 @@ import fs from "fs";
 //   });
 // }
 
-const file = fs.readFileSync(`public/images/logo.webp`);
+// const file = fs.readFileSync(`public/images/logo.webp`);
+const file = fs.readFileSync(
+  `/Users/enzobacqueyrisses/dev/enzo/portfolio/public/images/logo.webp`
+);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  try {
+    res.status(200).setHeader("Content-Type", "image/jpeg").send(file);
+  } catch (error) {
+    console.log(error);
+  }
   // await NextCors(req, res, {
   //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   //   origin: "*",
   //   optionsSuccessStatus: 200,
   // });
-  res.status(200).setHeader("Content-Type", "image/jpeg").send(file);
 }
