a7ee0270aa83910da74fb13588b7223df7cfb0a5
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 16:19:27 +0200

    Change CORS config

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -2,13 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 import fs from "fs";
 
-async function cors(req: NextApiRequest, res: NextApiResponse) {
-  await NextCors(req, res, {
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200,
-  });
-}
+// async function cors(req: NextApiRequest, res: NextApiResponse) {
+//   await NextCors(req, res, {
+//     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//     origin: "*",
+//     optionsSuccessStatus: 200,
+//   });
+// }
 
 const file = fs.readFileSync(`public/images/logo.webp`);
 
@@ -16,6 +16,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await cors(req, res);
+  // await NextCors(req, res, {
+  //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  //   origin: "*",
+  //   optionsSuccessStatus: 200,
+  // });
   res.status(200).setHeader("Content-Type", "image/jpeg").send(file);
 }
