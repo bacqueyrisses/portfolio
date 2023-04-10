8e5fb3595b547ecb2decfd25c938e88e124f8a7d
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 15:53:19 +0200

    Change image API path

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -4,5 +4,10 @@ import fs from "fs";
 const file = fs.readFileSync(`public/images/logo.webp`);
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).setHeader("Content-Type", "image/jpeg").send(file);
+  res
+    .status(200)
+    .setHeader("Content-Type", "image/jpeg")
+    .send(
+      "/Users/enzobacqueyrisses/dev/enzo/portfolio/public/images/logo.webp"
+    );
 }
