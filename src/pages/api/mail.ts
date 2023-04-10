38a3fda23251fea23ecef421f1044b009b2807a8
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 16:58:20 +0200

    Get mail info

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -8,5 +8,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log(req);
   res.status(200).setHeader("Content-Type", "image/webp").send(file);
 }
