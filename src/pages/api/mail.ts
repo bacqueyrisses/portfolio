e9b436c273816b44f06bc910ba59e217e70b0523
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-04-10 16:03:31 +0200

    Add CORS logic

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,9 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 import fs from "fs";
+
+const cors = Cors({
+  methods: ["GET"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const file = fs.readFileSync(`public/images/logo.webp`);
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await runMiddleware(req, res, cors);
   res
     .status(200)
     .setHeader("Allow", "GET")
