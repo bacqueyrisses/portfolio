cb5686c73c3cb859c053f7e73fe9338fb2eadeb4
Enzo <enzo.bacqueyrisses@gmail.com>
2023-07-23 19:22:46 +0200

    Update Technology.tsx

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -5,7 +5,6 @@ export default function Technology({technology}: TechnologyProps) {
         <a href={technology.link} target={"_blank"} rel="noreferrer"
            className="group transition duration-300">
             {technology.name}
-           // <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-[0.01rem] bg-primary opacity-90"></span>
         </a>
     );
 }
