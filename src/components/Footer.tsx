977d212bf3112ba421c2b34f7593e80536d06b36
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-23 05:42:28 +0200

    Update spacing on email icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,28 +25,10 @@ export default function Footer() {
         >
           Get in touch
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                 stroke="currentColor" className="ml-2 mt-0.5 w-4 h-4">
+                 stroke="currentColor" className="ml-2 sm:mt-0.5 w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round"
                       d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"/>
             </svg>
-
-            {/*<svg*/}
-          {/*  className="-mr-1 ml-2 mt-0.5 stroke-secondary stroke-2 dark:stroke-black"*/}
-          {/*  fill="none"*/}
-          {/*  width="10"*/}
-          {/*  height="10"*/}
-          {/*  viewBox="0 0 10 10"*/}
-          {/*  aria-hidden="true"*/}
-          {/*>*/}
-          {/*  <path*/}
-          {/*    className="opacity-0 transition group-hover:opacity-100"*/}
-          {/*    d="M0 5h7"*/}
-          {/*  ></path>*/}
-          {/*  <path*/}
-          {/*    className="transition group-hover:translate-x-[3px]"*/}
-          {/*    d="M1 1l4 4-4 4"*/}
-          {/*  ></path>*/}
-          {/*</svg>*/}
         </a>
       </div>
     </footer>
