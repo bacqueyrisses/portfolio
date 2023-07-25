2c3c274b50fd066de41687302e3ab302e37115fd
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-25 05:45:57 +0200

    Update technologies on hover

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -2,7 +2,7 @@ import {TechnologyProps} from "@/types";
 
 export default function Technology({technology}: TechnologyProps) {
     return (
-        <a href={technology.link} target={"_blank"} rel="noreferrer"
+        <a className={"sm:hover:opacity-95"} href={technology.link} target={"_blank"} rel="noreferrer"
         >
             {technology.name}
         </a>
