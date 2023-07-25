c945b6beec106469716918648e20e5d5cf71e3db
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-25 05:14:48 +0200

    Update metadescription

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                 <title>Enzo Bacqueyrisses / TypeScript Developer</title>
                 <meta
                     name="description"
-                    content="Full Stack JavaScript Developer living in Bordeaux / React & NodeJS"
+                    content="Full Stack TypeScript Developer / React & NodeJS"
                 />
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
