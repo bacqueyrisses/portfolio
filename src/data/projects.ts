2ef94d4647423cd6a9047b1259886626b553c5fc
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-31 20:35:13 +0200

    Update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,29 +1,29 @@
 export const projects = [
   {
     id: 1,
-    name: "Devzone",
+    name: "NextAI",
     description:
-      "Boost your development productivity with a custom dashboard. Get your information and tools in one place, letting you focus on what matters most.",
-    image: "bg-gradient1",
-    date: 2022,
-    link: "https://github.com/bacqueyrisses/devzone",
+        "Discover the Next.JS effortlessly with this AI-powered documentation search. Experience daily updates for the most up-to-date information.",
+    image: "bg-gradient2",
+    date: 2023,
+    link: "https://www.nextai.dev/",
   },
   {
     id: 2,
-    name: "Investy",
-    description:
-      "Easily venture into the world of investing with this intuitive tailored dashboard. The project offers a seamless experience for both users and admins.",
-    image: "bg-gradient2",
-    date: 2023,
-    link: "https://github.com/bacqueyrisses/",
-  },
-  {
-    id: 3,
     name: "Ouvrage",
     description:
       "Expand your vocabulary, discover new words, bolster your existing writing skills and get access to your favorites ones easily, all in one place.",
     image: "bg-gradient3",
     date: 2023,
     link: "https://www.ouvrage.dev/",
+  },
+  {
+    id: 3,
+    name: "Devzone",
+    description:
+        "Boost your development productivity with a custom dashboard. Get your information and tools in one place, letting you focus on what matters most.",
+    image: "bg-gradient1",
+    date: 2022,
+    link: "https://github.com/bacqueyrisses/devzone",
   },
 ];
