50f6887f6f5208c39338ce62065f9ba133ea728b
Enzo <enzo.bacqueyrisses@gmail.com>
2023-08-12 11:24:45 +0200

    Remove safari meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ export default function Home() {
           content="Full Stack TypeScript Developer / React & NodeJS"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#E1E6DE">
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
