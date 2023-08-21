525db73520dbed535495ab0d984c2fe329fc703b
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-22 00:32:24 +0200

    Update projects: Add PostAI

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,16 @@
 export const projects = [
   {
     id: 1,
+    name: "PostAI",
+    description:
+      "Design and send AI-crafted, location-based virtual postcards. Share personalized moments creatively!",
+    image: "bg-gradient1",
+    date: 2023,
+    link: "https://mypostai.vercel.app",
+    live: true,
+  },
+  {
+    id: 2,
     name: "NextAI",
     description:
       "Discover Next.JS effortlessly with this AI-powered documentation search. Experience daily updates for the most up-to-date information.",
@@ -10,7 +20,7 @@ export const projects = [
     live: true,
   },
   {
-    id: 2,
+    id: 3,
     name: "Ouvrage",
     description:
       "Expand your vocabulary, discover new words, bolster your existing writing skills and get access to your favorites ones easily, all in one place.",
@@ -18,15 +28,5 @@ export const projects = [
     date: 2023,
     link: "https://www.ouvrage.dev/",
     live: true,
-  },
-  {
-    id: 3,
-    name: "Devzone",
-    description:
-      "Boost your development productivity with a custom dashboard. Get your information and tools in one place.",
-    image: "bg-gradient1",
-    date: 2022,
-    link: "https://github.com/bacqueyrisses/devzone",
-    live: false,
   },
 ];
