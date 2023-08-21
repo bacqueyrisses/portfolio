d500d1f9b353fc2d87f54dd0a1e81333ac81f2b3
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-22 00:40:34 +0200

    Update projects order

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,22 +1,22 @@
 export const projects = [
   {
     id: 1,
-    name: "PostAI",
-    description:
-      "Design and send AI-crafted, location-based virtual postcards. Share personalized moments creatively!",
-    image: "bg-gradient1",
-    date: 2023,
-    link: "https://mypostai.vercel.app",
-    live: true,
-  },
-  {
-    id: 2,
     name: "NextAI",
     description:
       "Discover Next.JS effortlessly with this AI-powered documentation search. Experience daily updates for the most up-to-date information.",
     image: "bg-gradient2",
     date: 2023,
     link: "https://www.nextai.dev/",
+    live: true,
+  },
+  {
+    id: 2,
+    name: "PostAI",
+    description:
+      "Design and send AI-crafted, location-based virtual postcards. Share personalized moments creatively!",
+    image: "bg-gradient1",
+    date: 2023,
+    link: "https://mypostai.vercel.app",
     live: true,
   },
   {
