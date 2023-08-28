3ab07a80cc69c5c102f4ce333daad593752ee8b3
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-28 15:44:03 +0200

    Add theme-color meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import { satoshiFont } from "@/styles/fonts";
 export default function Document() {
   return (
     <Html lang="en" className={"scroll-smooth bg-secondary dark:bg-primary"}>
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#E1E6DE" />
+      </Head>
       <body
         className={`bg-secondary px-6 text-black dark:bg-primary dark:text-secondary lg:px-36`}
       >
