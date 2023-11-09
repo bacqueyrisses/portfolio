24fc22fc417201775f249200da6fda021fd1ea88
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-11-10 03:57:54 +0800

    Update hover colors on social links

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -8,7 +8,7 @@ export default function SocialLink({
 }) {
   return (
     <Link className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+      <Icon className="h-6 w-6 fill-zinc-500 group-hover:fill-zinc-700 dark:fill-zinc-400 dark:group-hover:fill-zinc-200" />
     </Link>
   )
 }
