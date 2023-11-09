831029a07b8c7fa98b57938fa3d664fecdb1fe19
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-11-10 03:49:01 +0800

    Simplify code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,8 +17,7 @@ export default function Projects({ focusedProject }: IProjects) {
           <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
             <div
               className={`absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition duration-500 ease-in-out group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl ${
-                focusedProject === project.id &&
-                'scale-100 opacity-100 transition duration-500 ease-in-out'
+                focusedProject === project.id && 'scale-100 opacity-100'
               }`}
             />
             <Link target={'_blank'} href={project.link}>
