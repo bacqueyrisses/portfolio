745c93dfc76f726b91787a5f22ea4f3d9d391eec
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-11-11 20:28:24 +0800

    Update hero description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,10 @@ export default async function Home() {
             Full-stack developer, typescript enthusiast and next.js lover.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I'm Enzo, my goal is to create optimized and beautiful my goal is to
-            create optimized and beautiful websites using the websites using the
-            latest technologies in the JavaScript full-stack ecosystem.
+            I'm Enzo, my goal is to create optimized and beautiful websites
+            using the latest technologies in the JavaScript full-stack
+            ecosystem. I'm passionate about open source projects and have
+            specialized in Next.js & TypeScript.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
