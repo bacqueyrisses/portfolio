30b02cf445dfb343ea54e646fdffe713b05a97d8
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-12-21 19:36:21 +0900

    Update postai image to gif

--- components/Websites.tsx
+++ components/Websites.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
-import clsx from "clsx"
-import nextai from "@/public/images/websites/nextai.webp"
-import postai from "@/public/images/websites/postai.webp"
-import ouvrage from "@/public/images/websites/ouvrage.webp"
-import { SetStateAction } from "react"
+import Image from 'next/image'
+import clsx from 'clsx'
+import nextai from '@/public/images/websites/nextai.webp'
+import postai from '@/public/images/websites/postai.gif'
+import ouvrage from '@/public/images/websites/ouvrage.webp'
+import { SetStateAction } from 'react'
 
 interface IWebsites {
   setFocusedProject: React.Dispatch<SetStateAction<number | null>>
 }
 export default function Websites({ setFocusedProject }: IWebsites) {
   const handleClick = (index: number) => {
-    const element = document.getElementById("projects-section")
-    element && element.scrollIntoView({ behavior: "smooth" })
+    const element = document.getElementById('projects-section')
+    element && element.scrollIntoView({ behavior: 'smooth' })
 
     setFocusedProject(index + 1)
 
@@ -19,6 +19,7 @@ export default function Websites({ setFocusedProject }: IWebsites) {
       setFocusedProject(null)
     }, 3000)
   }
+  const imageAltTexts = ['NextAI', 'postai', 'Ouvrage']
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex flex-col items-center justify-center gap-5 overflow-hidden px-4 py-4 sm:flex-row sm:gap-8">
@@ -27,12 +28,13 @@ export default function Websites({ setFocusedProject }: IWebsites) {
             onClick={() => handleClick(imageIndex)}
             key={image.src}
             className={clsx(
-              "relative aspect-[16/10] w-full flex-none overflow-hidden rounded-xl bg-zinc-50 dark:bg-zinc-800 sm:w-96 sm:rounded-2xl",
+              'relative aspect-[16/10] w-full flex-none overflow-hidden rounded-xl bg-zinc-50 dark:bg-zinc-800 sm:w-96 sm:rounded-2xl',
             )}
           >
             <Image
               src={image}
-              alt="enzo bacqueyrisses website"
+              unoptimized={true}
+              alt={`enzo bacqueyrisses ${imageAltTexts[imageIndex]}`}
               className="h-11/12 absolute inset-0 m-auto w-11/12 rounded-xl object-cover transition duration-500 ease-in-out  hover:scale-105"
             />
           </button>
