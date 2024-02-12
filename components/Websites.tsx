ba90e59b1e355fab954de659165affdb68fb6f4a
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-12 19:37:05 +0900

    Remove padding x on websites (mobile)

--- components/Websites.tsx
+++ components/Websites.tsx
@@ -22,7 +22,7 @@ export default function Websites({ setFocusedProject }: IWebsites) {
   const imageAltTexts = ['NextAI', 'postai', 'Ouvrage']
   return (
     <div className="mx-auto mt-20 max-w-7xl px-4 lg:mt-24 lg:px-8">
-      <div className="flex flex-col items-center justify-evenly gap-6 overflow-hidden px-4 sm:flex-row sm:px-10">
+      <div className="flex flex-col items-center justify-evenly gap-6 overflow-hidden sm:flex-row sm:px-10">
         {[nextai, postai, ouvrage].map((image, imageIndex) => (
           <button
             onClick={() => handleClick(imageIndex)}
