2e50ae15b3648b31106e4082890f800515970b95
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-15 14:30:42 +0900

    Update height dynamically on body

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <>
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="z-50 w-full rounded-t-[1.6rem]  bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
+          <div className="z-50 h-dvh w-full rounded-t-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
         </div>
       </div>
       <div className="relative flex w-full flex-col px-2.5 sm:px-0">
