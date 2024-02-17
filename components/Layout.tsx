33d591006b450abe4f725bb8a02e1992dbd38a32
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-17 15:53:15 +0900

    Update body background border radius

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,9 +9,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
-        className={`${
-          isSafariMobile ? 'absolute' : 'fixed'
-        } inset-0 flex justify-center sm:fixed sm:px-8`}
+        className={
+          'fixed left-0 right-0 top-0 flex h-lvh justify-center sm:px-8'
+        }
       >
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full rounded-t-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
