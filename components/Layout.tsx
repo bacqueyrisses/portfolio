4f977bbe499f8491f94b287897d69d2f25a1a93d
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-17 15:57:23 +0900

    Update height properties to fit safari scrolling behavior

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,7 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { headers } from 'next/headers'
-import { isOnSafariMobile } from '@/utils/safari'
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const isSafariMobile = isOnSafariMobile(headers())
-
   return (
     <>
       <div
@@ -13,8 +9,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
           'fixed left-0 right-0 top-0 flex h-lvh justify-center sm:px-8'
         }
       >
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full rounded-t-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
+        <div className="flex h-full w-full max-w-7xl lg:px-8">
+          <div className="h-full w-full rounded-t-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
         </div>
       </div>
       <div className="relative flex w-full flex-col px-2.5 sm:px-0">
