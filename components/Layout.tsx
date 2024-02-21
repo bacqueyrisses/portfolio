44719aaad7538c98e6a9f2aa237aecbad186c1bb
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-21 18:48:23 +0900

    fix: try to resolve Safari Mobile bug

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,8 +8,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <div className={"fixed inset-0 flex justify-center sm:px-8"}>
-        <div className="flex w-full max-w-7xl lg:px-8">
+      <div
+        className={
+          "fixed bottom-96 left-0 right-0 top-0 flex h-full justify-center sm:px-8"
+        }
+      >
+        <div className="flex h-full w-full max-w-7xl lg:px-8">
           <div className="h-lvh w-full rounded-t-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-t-3xl sm:border sm:border-zinc-800/5 sm:dark:border-white/5" />
         </div>
       </div>
