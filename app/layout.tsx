e6d4ab403f2f20f0568bbdf054daa3c57422b490
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-02-21 18:37:47 +0900

    fix: try to resolve Safari Mobile bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={"h-full antialiased"} suppressHydrationWarning>
-      <body className="bg-dotted relative flex h-full bg-zinc-100/70 dark:bg-black dark:bg-none">
+      <body className="bg-dotted flex h-full bg-zinc-100/70 dark:bg-black dark:bg-none">
         <Providers>
           <div className="flex h-lvh w-full">
             <Layout>{children}</Layout>
