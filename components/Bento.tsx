3ddd305cea9526bb62e9675d72bd430c3634ba07
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-05-23 00:00:35 +0200

    feat: make gradients look like earth

--- components/Bento.tsx
+++ components/Bento.tsx
@@ -69,23 +69,23 @@ const SkeletonOne = () => {
     >
       <motion.div
         variants={variants}
-        className="flex flex-row items-center space-x-2 rounded-full border border-neutral-100  bg-white p-2 dark:border-white/[0.2] dark:bg-black"
+        className="flex flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 dark:border-white/[0.2] dark:bg-black"
       >
-        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-pink-500 to-violet-500" />
+        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-blue-500 to-green-500" />
         <div className="h-4 w-full rounded-full bg-gray-100 dark:bg-neutral-900" />
-      </motion.div>
-      <motion.div
-        variants={variantsSecond}
-        className="ml-auto flex w-3/4 flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 dark:border-white/[0.2] dark:bg-black"
-      >
-        <div className="h-4 w-full rounded-full bg-gray-100 dark:bg-neutral-900" />
-        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-violet-500 to-pink-500" />
       </motion.div>
       <motion.div
         variants={variants}
         className="flex flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 dark:border-white/[0.2] dark:bg-black"
       >
-        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-pink-500 to-violet-500" />
+        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-blue-500 to-green-500" />
+        <div className="h-4 w-full rounded-full bg-gray-100 dark:bg-neutral-900" />
+      </motion.div>
+      <motion.div
+        variants={variants}
+        className="flex flex-row items-center space-x-2 rounded-full border border-neutral-100 bg-white p-2 dark:border-white/[0.2] dark:bg-black"
+      >
+        <div className="h-6 w-6 flex-shrink-0 rounded-full bg-gradient-to-r from-blue-500 to-green-500" />
         <div className="h-4 w-full rounded-full bg-gray-100 dark:bg-neutral-900" />
       </motion.div>
     </motion.div>
