6d85454bb1efd348de754e102b3344f2da85a52f
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-05-23 12:22:12 +0200

    feat: replace bmac logo by enzo logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,16 @@ import { ContainerInner, ContainerOuter } from "./Container";
 import Image from "next/image";
 import whiteCoffee from "@/public/images/logos/white-button.png";
 import darkCoffee from "@/public/images/logos/black-button.png";
+import avatarImage from "@/public/images/logos/logo.webp";
+import avatarDarkImage from "@/public/images/logos/logo-dark.webp";
 
 export function Footer() {
   return (
-    <footer className="mt-12 lg:mt-16 flex-none">
+    <footer className="mt-12 flex-none lg:mt-16">
       <ContainerOuter>
         <div className="border-t border-zinc-100 pb-10 pt-10 dark:border-zinc-700/40">
           <ContainerInner>
-            <div className="flex flex-col items-center justify-center gap-2">
+            <div className="flex flex-col items-center justify-center">
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Enzo Bacqueyrisses
               </p>
@@ -21,17 +23,17 @@ export function Footer() {
                 className={"cursor-pointer"}
               >
                 <Image
-                  src={whiteCoffee}
-                  width={100}
-                  height={100}
-                  alt={"Enzo Bacqueyrisses' Buy Me a Coffee logo"}
+                  src={avatarImage}
+                  width={40}
+                  height={40}
+                  alt={"Enzo Bacqueyrisses logo"}
                   className={"block dark:hidden"}
                 />
                 <Image
-                  src={darkCoffee}
-                  width={100}
-                  height={100}
-                  alt={"Enzo Bacqueyrisses' Buy Me a Coffee logo dark mode"}
+                  src={avatarDarkImage}
+                  width={40}
+                  height={40}
+                  alt={"Enzo Bacqueyrisses logo dark mode"}
                   className={"hidden dark:block"}
                 />
               </Link>
