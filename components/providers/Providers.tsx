fe7aa1cd2714ba2dd7059394e2b21a3e5f399ca6
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-06-18 00:08:49 +0200

    feat: make system as default theme

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -47,7 +47,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <AppContext.Provider value={{ previousPathname }}>
-      <ThemeProvider attribute="class" defaultTheme="light">
+      <ThemeProvider attribute="class" defaultTheme="system">
         <ThemeWatcher />
         {children}
         <Analytics />
