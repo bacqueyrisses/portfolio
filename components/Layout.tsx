c37603bde10c9715ff0c505e570d72d69cc24221
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2026-06-24 23:30:32 +0700

    feat: remove rounded corner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/Header";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="relative flex h-fit flex-col rounded-[1.6rem] bg-white dark:bg-zinc-900 sm:rounded-2xl sm:border sm:border-zinc-200/20 sm:dark:border-zinc-700/40">
+      <div className="relative flex h-fit flex-col bg-white dark:bg-zinc-900 sm:border-x sm:border-zinc-200/20 sm:dark:border-zinc-700/40">
         <Header />
         <main className="flex-auto">{children}</main>
         <Footer />
